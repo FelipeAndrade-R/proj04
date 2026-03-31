bcc24f4fc0d34412e130ab1642a2476a0a9e9036
FelipeAndrade-R <felipeka07@gmail.com>
2026-03-31 11:09:59 -0300

    ex01

---
+++ ex05/script.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Escreva o número:"));
+
+if (numero % 5 == 0) {
+    alert("Esse número é multiplo de 5");
+} else {
+    alert("Esse número não é múltiplo de 5");
+}
+
+
